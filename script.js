01dbbde560a1722bed3f840323c9dd65f975ef25
Finn <202270766+finnjng@users.noreply.github.com>
2025-03-16 01:06:36 -0500

    Completed rock-paper-scissors exercise

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Test");
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    return randomNumber == 1 ? "rock" :
+        randomNumber == 2 ? "paper" :
+            "scissors";
+};
+
+const getHumanChoice = () => prompt("Enter Rock, Paper or Scissors!").toLowerCase();
+
+const playRound = (humanChoice, computerChoice) => {
+
+    if(humanChoice == computerChoice){
+        console.log(`Its a Tie! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} ties ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} !`);
+        return;
+    }
+    if((humanChoice == "rock" && computerChoice == "scissors")||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice =="scissors" && computerChoice == "paper")){
+        humanScore++;
+        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
+    }else{
+        computerScore++
+        console.log(`You Lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`);
+    }
+};
+
+while (round <= 5) {
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Round: ${round}, Your score: ${humanScore} , Computer score: ${computerScore}`);
+    round++;
+}
